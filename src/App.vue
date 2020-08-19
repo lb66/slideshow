f8a3ff0e85a063332336dae69b8c9e1c7e7aba97
lb66 <807242867@qq.com>
2020-08-19 17:08:11 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SlideShow />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SlideShow from "./components/SlideShow";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SlideShow,
+  },
+};
 </script>
 
-<style lang="scss">
+<style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/SlideShow.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="slideShow-wrap" @mouseenter="stop" @mouseleave="begin">
+    <transition-group tag="ul" class="slide-ul" name="list">
+      <li v-for="(list,index) in slideList" :key="index" v-show="index===currentIndex">
+        <img :src="list.image" />
+      </li>
+    </transition-group>
+    <div class="slideShow-items">
+      <span
+        v-for="(item,index) in slideList.length"
+        :key="index"
+        :class="{'active':index===currentIndex}"
+        @click="change(index)"
+      ></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SlideShow",
+  data() {
+    return {
+      slideList: [
+        { image: require("@/assets/img/1.jpg") },
+        { image: require("@/assets/img/2.jpg") },
+        { image: require("@/assets/img/3.jpg") },
+      ],
+      currentIndex: 0,
+      timer: "",
+    };
+  },
+  methods: {
+    change(index) {
+      this.currentIndex = index;
+    },
+    autoPlay() {
+      this.currentIndex++;
+      if (this.currentIndex > this.slideList.length - 1) {
+        this.currentIndex = 0;
+      }
+    },
+    begin() {
+      this.timer = setInterval(() => {
+        this.autoPlay();
+      }, 4000);
+    },
+    stop() {
+      clearInterval(this.timer), (this.timer = "");
+    },
+  },
+  created() {
+    this.begin();
+  },
+};
+</script>
+
+<style scoped lang='scss'>
+* {
+  padding: 0;
+  margin: 0;
+}
+.slideShow-wrap {
+  position: relative;
+  height: 450px;
+  width: 100%;
+  overflow: hidden;
+  background-color: #fff;
+}
+.slide-ul {
+  width: 100%;
+  height: 100%;
+  li {
+    list-style: none;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+}
+.slideShow-items {
+  position: absolute;
+  top: 400px;
+  width: 100%;
+  text-align: center;
+  span {
+    display: inline-block;
+    height: 8px;
+    width: 32px;
+    margin: 3px;
+    background-color: #b2b2b2;
+    cursor: pointer;
+  }
+  .active {
+    background-color: orange;
+  }
+}
+.list-enter-active {
+  transition: all 2s;
+}
+.list-leave-active {
+  transform: translateX(-80%);
+  transition: all 2s;
+}
+.list-enter {
+  opacity: 0;
+  transform: translateX(100%);
+}
+.list-leave {
+  transform: translateX(0);
+}
+.list-leave-to {
+  opacity: 0;
+}
+</style>
